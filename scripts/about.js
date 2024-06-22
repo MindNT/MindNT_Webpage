285e4967c2c5ff4f7b450d147f70b41be47d4e76
Adrian Carmona Rodriguez <adriancarmonarodriguez@MacBook-Pro-de-Adrian.local>
2024-06-22 16:49:51 -0600

    About us ES in progress...

---
+++ scripts/about.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const languageToggle = document.getElementById('languageToggle');
+    const isEnglish = localStorage.getItem('isEnglish') === 'true';
+
+    if (isEnglish) {
+        languageToggle.checked = true;
+    } else {
+        languageToggle.checked = false;
+    }
+
+    // Add event listener for toggle switch
+    languageToggle.addEventListener('change', function() {
+        if (this.checked) {
+            localStorage.setItem('isEnglish', 'true');
+            window.location.href = 'aboutEN.html'; // Asegúrate de que esta URL sea correcta
+        } else {
+            localStorage.setItem('isEnglish', 'false');
+            window.location.href = 'about.html'; // Asegúrate de que esta URL sea correcta
+        }
+    });
+});
